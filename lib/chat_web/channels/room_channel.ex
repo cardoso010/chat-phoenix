0f24d7dede7aa519c352c89cdb26e31860bf5a63
Gabriel Cardoso Luiz <gabriel.cardoso010@gmail.com>
2019-10-19 18:55:46 -0300

    added when anyone came in chat, it return history of the conversation and save history

--- lib/chat_web/channels/room_channel.ex
+++ lib/chat_web/channels/room_channel.ex
@@ -3,6 +3,7 @@ defmodule ChatWeb.RoomChannel do
 
   def join("room:lobby", payload, socket) do
     if authorized?(payload) do
+      send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -18,12 +19,26 @@ defmodule ChatWeb.RoomChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (room:lobby).
   def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
+    Chat.Message.changeset(%Chat.Message{}, payload) |> Chat.Repo.insert()
+    broadcast(socket, "shout", payload)
     {:noreply, socket}
   end
 
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
+  end
+
+  def handle_info(:after_join, socket) do
+    Chat.Message.get_messages()
+    |> Enum.each(fn msg ->
+      push(socket, "shout", %{
+        name: msg.name,
+        message: msg.message
+      })
+    end)
+
+    # :noreply
+    {:noreply, socket}
   end
 end
