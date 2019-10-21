e16c9573ed824aaf2f6ae44a0ec4d804bc43dab7
Gabriel Cardoso Luiz <gabriel.cardoso010@gmail.com>
2019-10-21 12:07:04 -0300

    Config heroku

--- config/prod.exs
+++ config/prod.exs
@@ -11,10 +11,14 @@ use Mix.Config
 # before starting your production server.
 config :chat, ChatWeb.Endpoint,
   load_from_system_env: true,
+  http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "chat-phx.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+# Do not print debug messages in production
+config :logger, level: :info
 
 # Configure your database
 config :chat, Chat.Repo,
@@ -22,9 +26,6 @@ config :chat, Chat.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
-
-# Do not print debug messages in production
-config :logger, level: :info
 
 # ## SSL Support
 #
